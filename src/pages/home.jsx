7c188bca3e1f545b04dbd2993a5ace0ceb3d7111
Wakjira-Tesama <wakjiratesama@gmail.com>
2025-10-27 23:00:51 +0300

    add new file axios

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./../componantes/header.jsx";
 import Footer from "../componantes/footer.jsx";
+import Banner from "../componantes/banner.jsx";
 function Home() {
   return (
     <div>
       <Header />
+      <Banner />
       <Footer />
     </div>
   );
